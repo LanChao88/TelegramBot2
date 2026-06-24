67ca829ea90bccee95190077e3f63433e62e8ddb
LanChao88 <duydz0934@gmail.com>
2026-06-24 13:22:32 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,61 @@
+from telegram import Update
+import re
+from telegram.ext import Application, MessageHandler, filters
+
+TOKEN = "8315910144:AAE2Wext-J09UZbEweaAS9DiYrTo4SPs0HY"
+
+async def calc(update, context):
+
+    text = update.message.text.strip()
+
+    # 地址功能
+    if text in ["地址", "结算", "钱包"]:
+
+        address = "TP88hmjvfxcvW4xQg3r53j5iTZfXXuuTDr"
+
+        msg = f"""
+ 亓亓唯一收款地址
+
+<code>{address}</code>
+
+🔝点击地址可自动复制🔝
+
+⚠️前6位：{address[:6]}
+⚠️中6位：{address[6:12]}
+⚠️后6位：{address[-6:]}
+
+⚠️烦请核对好，若有账户差异请勿打入。
+避免转到钓鱼山寨地址。
+"""
+
+        await update.message.reply_text(
+    msg,
+    parse_mode="HTML"
+)
+        return
+
+    # 加减乘除
+    if not re.fullmatch(r'\d+[+\-*/]\d+', text):
+        return
+
+    try:
+        result = eval(text)
+
+        if isinstance(result, float):
+            if result.is_integer():
+                result = int(result)
+
+        await update.message.reply_text(str(result))
+
+    except:
+        pass
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(
+    MessageHandler(filters.TEXT, calc)
+)
+
+print("机器人启动成功")
+
+app.run_polling()
